48bf84a84f9395ec70abeed1cb160801409114b9
LBKhoa <152173511+LBKhoa@users.noreply.github.com>
2024-01-22 23:00:05 +0700

    Create Lab1Bai4.java

---
+++ Lab1_63134331/src/ntu/Lab1Bai4.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package ntu;
+import java.util.Scanner;
+/**
+ * 
+ */
+public class Lab1Bai4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//khai bao bien
+		double a, b, c, delta;
+		
+		//Nhap cac he so a b c tu ban phim
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhâp a: ");
+		a = scanner.nextDouble();
+		System.out.print("Nhâp b: ");
+		b = scanner.nextDouble();
+		System.out.print("Nhâp c: ");
+		c = scanner.nextDouble();
+		
+		//Tinh delta
+		delta = Math.pow(b, 2)- 4 * a * c;
+		
+		//kiem tra delta va xuat ra man hinh
+		
+	}
+
+}
