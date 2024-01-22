c79b4b9e2bb354835fd963b590738ada9d07917c
LBKhoa <152173511+LBKhoa@users.noreply.github.com>
2024-01-22 22:36:35 +0700

    Update Lab1Bai2.java

--- Lab1_63134331/src/ntu/Lab1Bai2.java
+++ Lab1_63134331/src/ntu/Lab1Bai2.java
@@ -2,7 +2,7 @@
  * 
  */
 package ntu;
-
+import java.util.Scanner;
 /**
  * 
  */
@@ -12,11 +12,25 @@ public class Lab1Bai2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// khai bao bien
+		double dai, rong, chuVi, dienTich, canhNhoNhat;
 		
-		Scanner banPhim = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập chiều dài của hình chữ nhật: ");
+		dai = scanner.nextDouble();
+		System.out.print("Nhập chiều rộng của hình chữ nhật:");
+		rong = scanner.nextDouble();
 		
 		
+		// tinh chu vi hinh chư nhat, dien tich, canh nho nhat
+		chuVi = (dai + rong)*2;
+		dienTich = dai * rong;
+		canhNhoNhat = Math.min(dai,rong);
+		
+		//xuat ket qua
+		System.out.println("Chu vi hình chữ nhật là: " + chuVi);
+		System.out.println("Diện tích hình chữ nhật là: " + dienTich);
+		System.out.println("Cạnh nhỏ nhất hình chữ nhật là: " + canhNhoNhat);
 	}
 
 }
